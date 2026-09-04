a95f35f4f2cf8c4a93dfb0fe9433ce2066ed6001
TanwarRahul21 <tanwarrahul9982@gmail.com>
2026-09-05 01:34:51 +0530

    Add reverse string solution

---
+++ 344.reverse-string.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=344 lang=javascript
+ *
+ * [344] Reverse String
+ */
+
+// @lc code=start
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let answer = [];
+    answer.push(s.reverse());
+    return answer;
+    
+};
+// @lc code=end
+
